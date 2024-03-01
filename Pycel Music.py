a5be9bf0c8c95b22fc662ab3777ac04fad13d0ae
Andrew Simpson <absimp01@gmail.com>
2024-02-29 22:01:25 -0500

    Added functions to split, find most commonly occuring artist of dataset

--- Pycel Music.py
+++ Pycel Music.py
@@ -1,5 +1,22 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
 
-df = pd.read_excel(r"C:\\Users\\absim\\OneDrive\\Documents\\SpotifyData.xlsx")
+# create datafram for spotifydata.xlsx
 
-print(df)
+df = pd.read_excel(r"C:\\Users\\absim\\OneDrive\\Documents\\SpotifyData.xlsx", usecols="A")
+
+# dataframe to str() to enable splitting
+
+spotifyData = str(df)
+
+returnList = spotifyData.split() # split the list
+Counter = Counter(returnList)
+topFive = Counter.most_common(5)
+
+# store values from artist column into list
+# most common function (5)
+# num songs attributed to top (5) artists in top 50 
+
+# plt.bar(topFive)
